b6f2884360f6ce92bf6f9d94d980ab37727ba760
Fauno Guazina <prof.guazina@gmail.com>
2025-09-28 19:58:26 +0200

    [FIX] hateoas relation after testing

--- src/main/java/com/capitole/technicaltest/catalog/adapters/inbound/rest/dto/ProductResponse.java
+++ src/main/java/com/capitole/technicaltest/catalog/adapters/inbound/rest/dto/ProductResponse.java
@@ -2,8 +2,10 @@ package com.capitole.technicaltest.catalog.adapters.inbound.rest.dto;
 
 import java.math.BigDecimal;
 import lombok.Builder;
+import org.springframework.hateoas.server.core.Relation;
 
 @Builder
+@Relation(collectionRelation = "content")
 public record ProductResponse(
     String sku,
     String description,
